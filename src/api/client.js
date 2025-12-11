d8f26c6cd68578bf932ecc2ab9a37cbe29ffdec9
Akuro1411 <nahidisayev147@gmail.com>
2025-12-11 16:27:50 +0300

    Client.js is updated

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,14 @@
+// src/api/client.js
 import axios from "axios";
- const API_BASE = import.meta.env.VITE_API_BASE_URL?.replace(/\/$/, "") || "https://aegeeapp2-eabxfecshjbacmau.westeurope-01.azurewebsites.net"; 
- console.log("API_BASE in client.js =", API_BASE); 
- const api = axios.create({ baseURL: '/api', }); 
- 
- export default api;
+
+const API_BASE =
+  import.meta.env.VITE_API_BASE_URL?.replace(/\/$/, "") ||
+  "https://aegeeapp2-eabxfecshjbacmau.westeurope-01.azurewebsites.net";
+
+console.log("API_BASE in client.js =", API_BASE);
+
+const api = axios.create({
+  baseURL: ${API_BASE}/api,   // -> https://...azurewebsites.net/api
+});
+
+export default api;
